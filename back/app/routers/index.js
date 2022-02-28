c7c2d6c473dec0d5837962bf5f7964a3ce52848d
LudovicRougier <ludovic.rougier.1993@gmail.com>
2022-02-28 17:48:32 +0100

    polishing what we added today

--- back/app/routers/index.js
+++ back/app/routers/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 
 const controller = require('../controllers');
+const controllerHandler = require('../helpers/apiControllerHandler');
 const { errorHandler } = require('../helpers/errorHandler');
 
 const router = express.Router();
+
 /**
 * GET /employee
 * @summary Get all employees
 * @tags Employee
 * @return {Employee} 200 - success response - application/json
 */
-router.get('/employee', controller.getAllEmployee);
+router.get('/employee', controllerHandler(controller.getAllEmployee));
 
 router.use((err, _, response, next) => {
   errorHandler(err, response, next);
