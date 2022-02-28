1937b83524a241e3387cf33ad7f1049ea43833f2
Mustapha HAMADI <hamadi.mustapha7@gmail.com>
2022-02-28 11:18:01 +0100

    add Btn component and import for additionnal mui/lab

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import Btn from '../Btn/Btn';
 const App = () => {
-  return <div>O'rganizer</div>;
+  const handleBtn = () => {
+    console.log('from parent component');
+  };
+
+  return (
+    <div>
+      <Btn text='Valider' clicked={handleBtn} />
+    </div>
+  );
 };
 
 export default App;
