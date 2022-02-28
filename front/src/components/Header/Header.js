de037167b1c92d20d89594e88d4fcd749c02b456
Chloe Thomas <chloe.thomas.92@gmail.com>
2022-02-28 14:38:29 +0100

    Add style folders

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+// import styles
 import './header.scss';
 import LogoutIcon from '@mui/icons-material/Logout';
 
-const Header = ({logoutButton}) => {
+const Header = ({logout}) => {
   return (
     <div className='header'>
+        {/* // TODO: ajout du logo à la place du titre */}
+
         <h1 className='header__title'>O'rganizer</h1>
-        <button className='header__logout-button'>
+        <button onClick={logout} className='header__logout-button'>
           <LogoutIcon />
         </button>
     </div>
   )
 }
 
-Header.propTypes = {}
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+}
 
 export default Header
