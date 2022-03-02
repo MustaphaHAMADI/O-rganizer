5e0a7362ea3f998419c7ae806cbb7c14eb56f763
Mustapha HAMADI <hamadi.mustapha7@gmail.com>
2022-03-02 17:13:48 +0100

    add toastify main notifications

--- front/src/index.js
+++ front/src/index.js
@@ -4,11 +4,14 @@ import App from './components/App/App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ToastContainer />
       <Provider store={store}>
         <Routes>
           <Route path='/*' element={<App />} />
