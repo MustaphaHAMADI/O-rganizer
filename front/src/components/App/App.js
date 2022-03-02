27e69ee3b3e308384cae3260685fc8beda1e5b83
Hassane <touhassan@gmail.com>
2022-03-02 15:19:42 +0100

    Realisation Page 404

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -8,6 +8,7 @@ import Footer from '../Footer/Footer';
 import Planning from '../Planning/Planning';
 import RequireAuth from '../RequireAuth/RequireAuth';
 import NoAuthRequired from '../NoAuthRequired/NoAuthRequired';
+import NotFound from '../NotFound/NotFound';
 
 // import style
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -36,6 +37,7 @@ const App = () => {
           <Route element={<RequireAuth />}>
             <Route path='/planning' element={<Planning />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </ThemeProvider>
