e1b6810a73eec69114c43c66d4680c4c903e703d
Chloe Thomas <chloe.thomas.92@gmail.com>
2022-03-03 13:39:57 +0100

    Creation of accordion button

--- front/src/components/Team/Team.js
+++ front/src/components/Team/Team.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './team.scss';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Team = ({
     teamName,
     teamMembers,
 }) => {
-  return (
-    <div className='team'>
-        <h3 className='team__title'>{teamName}</h3>
-        <ul className='team__members'>
-            {
-            teamMembers.map((member) => (
-                <li className='team__member' key={member.id}>
-                    {member.firstName} {member.lastName}
-                </li>
-            ))
-            }
-        </ul>
-    </div>
-  )
+    const [expanded, setExpanded] = React.useState(false);  
+    const handleChange = (panel) => (event, isExpanded) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+
+    return (
+        <div className='team'>
+            <h3 className='team__title'>{teamName}</h3>
+            <ul className='team__members--desktop'>
+                {teamMembers.map((member) => (
+                    <li className='team__member' key={member.id}>
+                        {member.firstName} {member.lastName}
+                    </li>
+                ))}
+            </ul>
+
+            <div className='team__members--mobile'>
+                <Accordion 
+                    expanded={expanded === 'panel'}
+                    onChange={handleChange('panel')} 
+                    elevation={0} 
+                    sx={{backgroundColor:'transparent'}}>
+                    <AccordionSummary
+                      aria-controls="panel1bh-content"
+                      id="panel1bh-header"
+                      expandIcon={<ExpandMoreIcon />}
+                    >
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                        {teamMembers.map((member) => (
+                            <li className='team__member' key={member.id}>
+                                {member.firstName} {member.lastName}
+                            </li>
+                        ))}
+                      </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+        </div>
+    )
 }
 
 Team.propTypes = {
