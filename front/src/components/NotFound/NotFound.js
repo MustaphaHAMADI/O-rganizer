c7858ebd94a0af17b33acde5b1e8600232a1fc03
Hassane <touhassan@gmail.com>
2022-03-03 14:01:59 +0100

    update 404

--- front/src/components/NotFound/NotFound.js
+++ front/src/components/NotFound/NotFound.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import './header.scss';
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './NotFound.scss';
 
 const NotFound = () => {
     return ( 
     <div className="not-found">
         <h1>404</h1>
-        <
-        <NavLink to="/"> Retourner à l'accueil </NavLink>
-        </div>;
+        
+        <Link to="/"> Retourner à l'accueil </Link>
+
+    </div>
+    )
+    
+}
+
 
 
 export default React.memo(NotFound);
