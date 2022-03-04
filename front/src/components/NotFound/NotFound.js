90827dd852b2e0d7ee0fc923ef91d0ea5721df5f
Hassane <touhassan@gmail.com>
2022-03-04 10:00:18 +0100

    4 update

--- front/src/components/NotFound/NotFound.js
+++ front/src/components/NotFound/NotFound.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './NotFound.scss';
-import Btn from '../Btn/Btn'
-
+import Btn from '../Btn/Btn';
+import './NotFound.jpg';
 
 
 
 const NotFound = () => {
     return ( 
-    <div className="not-found">
+    <div className="NotFound">
         <h1>404</h1>
         <Link to="/"><button type="button" className> 
-        
         <Btn text='Retourner à laccueil' />
         </button>
         </Link>
-    
+        <img src={NotFound} height="500" alt="NotFound"/>
 
 
     </div>
