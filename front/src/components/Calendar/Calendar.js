f3390da0865fae106fd34067ae41640cd33bacce
Chloe Thomas <chloe.thomas.92@gmail.com>
2022-03-04 13:34:29 +0100

    Creation of Calendar components (empty component)

---
+++ front/src/components/Calendar/Calendar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './calendar.scss';
+
+const Calendar = (props) => {
+  return (
+    <div className='calendar'>
+
+    </div>
+  )
+}
+
+Calendar.propTypes = {}
+
+export default Calendar
