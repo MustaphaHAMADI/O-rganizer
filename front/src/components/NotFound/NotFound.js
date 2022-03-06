0d4593e9374249cdc862187dcbfb511a23165fea
Hassane <touhassan@gmail.com>
2022-03-06 17:14:07 +0100

    last update

--- front/src/components/NotFound/NotFound.js
+++ front/src/components/NotFound/NotFound.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './NotFound.scss';
 import Btn from '../Btn/Btn';
-import NotFoundImage from './NotFoundImage.jpg';
+import NotFoundImage from './NotFoundImage.png';
 
 
 
 const NotFound = () => {
     return ( 
-    <div className="not-found">
- 
- <img src={NotFoundImage} height="500" width="500" alt="not-found"/>
+        <div className="not-found"  
+
+        
+
+  style={{ backgroundImage: `url(${NotFoundImage/NotFoundImage.png})`,
+
+        height: "700px", width: "700", alt:"not-found",
+        backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
+
+
+  }}>
         
         <Link to="/"> 
-        <Btn text='Retourner à laccueil'/>
+        <Btn text="Retourner à l' accueil"/>
         </Link>
     
        
@@ -27,3 +35,5 @@ const NotFound = () => {
 
 export default React.memo(NotFound);
 
+/*<img src={NotFoundImage} height="500" width="500" alt="not-found"/>*/
+        
