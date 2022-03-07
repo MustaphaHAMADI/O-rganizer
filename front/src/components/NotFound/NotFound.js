3881bc571edfa9944cd4cf14d37523e75c04b067
Hassane <touhassan@gmail.com>
2022-03-07 09:38:16 +0100

    last2update

--- front/src/components/NotFound/NotFound.js
+++ front/src/components/NotFound/NotFound.js
@@ -2,24 +2,14 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './NotFound.scss';
 import Btn from '../Btn/Btn';
-import NotFoundImage from './NotFoundImage.png';
+
 
 
 
 const NotFound = () => {
     return ( 
-        <div className="not-found"  
+        <div className="not-found" > 
 
-        
-
-  style={{ backgroundImage: `url(${NotFoundImage/NotFoundImage.png})`,
-
-        height: "700px", width: "700", alt:"not-found",
-        backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'
-
-
-  }}>
-        
         <Link to="/"> 
         <Btn text="Retourner à l' accueil"/>
         </Link>
