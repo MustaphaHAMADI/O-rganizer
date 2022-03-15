9c010600b5c1a37c4a918d95685b8c437a89e0e2
Chloe Thomas <chloe.thomas.92@gmail.com>
2022-03-15 10:46:03 +0100

    Add menu burger on Header

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,12 +1,14 @@
 // import dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
 // import styles
 import './header.scss';
-import { Button } from '@mui/material';
+import { Button, Menu, MenuItem } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
+import MenuIcon from '@mui/icons-material/Menu';
 
 // import elements
 import Btn from '../Btn/Btn';
@@ -14,6 +16,26 @@ import logo from '../../assets/logo.png';
 import userAvatar from '../../assets/user.png';
 
 const Header = ({ logout, isLoggedIn, user }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 1000 });
+    return isDesktop ? children : null;
+  };
+
+  const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 999 });
+    return isMobile ? children : null;
+  };
+
   return (
     <div className='header'>
       {!isLoggedIn && (
@@ -22,7 +44,6 @@ const Header = ({ logout, isLoggedIn, user }) => {
           <h1 className='header__banner-title'>O'rganizer</h1>
         </div>
       )}
-      <div className='header__btns--desktop'>
         {isLoggedIn && (
           <div className='header__logged'>
             <div className='header__user-info'>
@@ -43,6 +64,8 @@ const Header = ({ logout, isLoggedIn, user }) => {
                 </p>
               </div>
             </div>
+
+            <Desktop>
             <div className='header__buttton--container'>
               {user.role === 'admin' ? (
                 <React.Fragment>
@@ -60,31 +83,53 @@ const Header = ({ logout, isLoggedIn, user }) => {
                 clicked={logout}
               />
             </div>
-          </div>
-        )}
-      </div>
+            </Desktop>
 
-      <div className='header__btns--mobile'>
-        {isLoggedIn && (
-          <div className='header__logged'>
-            <div className='header__user-info'>
-              <NavLink to={`/user/${JSON.parse(localStorage.user).id}`}>
-                <img className='header__avatar' src={user} alt='User avatar' />
-              </NavLink>
-              <div className='header__user-info-details'>
-                <p className='header__user-info-number'>
-                  {JSON.parse(localStorage.user).reg_number}
-                </p>
-                <p className='header__user-info-name'>
-                  {JSON.parse(localStorage.user).name}{' '}
-                  {JSON.parse(localStorage.user).lastname}
-                </p>
-              </div>
-            </div>
-            <Btn text='' icon={<LogoutIcon />} clicked={logout} />
+            <Mobile>
+              {user.role === 'admin' ? (
+                <div className='header__menu'>
+                  <Button
+                    id="demo-positioned-button"
+                    aria-controls={open ? 'demo-positioned-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                  >
+                    <MenuIcon fontSize='large'/>
+                  </Button>
+                  <Menu
+                    id="demo-positioned-menu"
+                    aria-labelledby="demo-positioned-button"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                      vertical: 'center',
+                      horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                      vertical: 'center',
+                      horizontal: 'left',
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <Link to='users'>Gestion des utilisateurs</Link>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>Gestion des factions</MenuItem>
+                    <MenuItem onClick={logout}>Se déconnecter</MenuItem>
+                  </Menu>
+                </div>
+              ) :
+                <Btn
+                  icon={<LogoutIcon />}
+                  clicked={logout}
+                />
+              }
+
+            </Mobile>
           </div>
         )}
-      </div>
+
     </div>
   );
 };
